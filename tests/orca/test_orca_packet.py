d06ec9bff7eaf08701ddddca3d54ba92acf35e02
Jason Detwiler <jasondet@uw.edu>
2023-10-19 03:23:58 +0100

    switch to new file and add some more tests

--- tests/orca/test_orca_packet.py
+++ tests/orca/test_orca_packet.py
@@ -5,12 +5,31 @@ def test_orca_packet_funcs(orca_stream):
     # The values in this test are particular to the test orca file in
     # legend-testdata and may need to be changed if that file is changed
 
-    assert orca_stream.count_packets() == 72
+    assert orca_stream.count_packets() == 911
 
     packet = orca_stream.load_packet()
     assert orca_packet.is_short(packet) == False
-    assert orca_packet.get_data_id(packet) == 7
+    assert orca_packet.get_data_id(packet) == 3
     assert orca_packet.get_n_words(packet) == 4
-    assert orca_packet.hex_dump(packet, return_output=True)[-1] == "3 0x6286930b"
+    assert orca_packet.hex_dump(packet, return_output=True)[-1] == '3 0x63c1977a'
+
+    id_dict = orca_stream.header.get_id_to_decoder_name_dict()
+    seen = []
+    for ii in range(100):
+        packet = orca_stream.load_packet(ii)
+        if packet is None: break
+        name = id_dict[orca_packet.get_data_id(packet)]
+        if ii == 0: assert name == 'OrcaHeaderDecoder'
+        if ii == 1: assert name == 'ORRunDecoderForRun'
+        if ii == 911: assert name == 'ORRunDecoderForRun'
+        if name not in seen: seen.append(name)
+    expected = [
+        'OrcaHeaderDecoder', 
+        'ORRunDecoderForRun', 
+        'ORFlashCamListenerConfigDecoder', 
+        'ORFlashCamListenerStatusDecoder', 
+        'ORFlashCamWaveformDecoder'
+    ]
+    assert seen == expected
 
     orca_stream.close_stream()  # avoid warning that file is still open
